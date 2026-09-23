2f5c53eea8ae5bf28cdd6e5bd3e329486ec24bf6
Alexis <alexis.villalobos27@gmail.com>
2026-09-22 22:11:20 -0700

    fix: use loadable sudoers rule filename

--- authorize-macfan.zsh
+++ authorize-macfan.zsh
@@ -5,7 +5,7 @@ readonly helper_dir="/Library/PrivilegedHelperTools"
 readonly helper="${helper_dir}/com.alexis.macfan.helper"
 readonly binary="${helper_dir}/macfan"
 readonly rule_dir="/etc/sudoers.d"
-readonly rule="${rule_dir}/com.alexis.macfan"
+readonly rule="${rule_dir}/macfan-authorized"
 readonly sudoers="/etc/sudoers"
 
 [[ "$EUID" == 0 ]] || exit 77
@@ -14,9 +14,9 @@ readonly sudoers="/etc/sudoers"
 /usr/bin/install -o root -g wheel -m 755 /Users/alexis/bin/macfan "$binary"
 /usr/bin/install -o root -g wheel -m 755 /Users/alexis/src/macfan/macfan-privileged-helper.zsh "$helper"
 /usr/bin/install -d -o root -g wheel -m 755 "$rule_dir"
-/usr/bin/install -o root -g wheel -m 440 /Users/alexis/src/macfan/com.alexis.macfan.sudoers "$rule"
+/usr/bin/install -o root -g wheel -m 440 /Users/alexis/src/macfan/macfan-authorized "$rule"
 
-if ! /usr/bin/grep -Eq '^[[:space:]]*@includedir[[:space:]]+/etc/sudoers\.d[[:space:]]*$' "$sudoers"; then
+if ! /usr/bin/grep -Eq '^[[:space:]]*@includedir[[:space:]]+(/private)?/etc/sudoers\.d[[:space:]]*$' "$sudoers"; then
     readonly temporary="$(/usr/bin/mktemp /tmp/macfan-sudoers.XXXXXX)"
     /bin/cp "$sudoers" "$temporary"
     /bin/printf '\n@includedir /etc/sudoers.d\n' >> "$temporary"
